eefac2b465b8452086a2e62fb4d2e5a87eeec243
MpokiAbel <mpokiabel@gmail.com>
2023-04-14 16:07:15 +0200

    splice

--- fuse_fs/stackfs.c
+++ fuse_fs/stackfs.c
@@ -6,8 +6,9 @@
 #include <string.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <stdlib.h>
 
-static const char *base_dir = "/home";
+static const char *base_dir = "/home/mpoki/Documents/M2_MOSIG/Stage/fuse_fs/root";
 
 int stackfs__getattr(const char *path, struct stat *stat, struct fuse_file_info *fi)
 {
@@ -17,8 +18,8 @@ int stackfs__getattr(const char *path, struct stat *stat, struct fuse_file_info 
     (void)fi;
 
     sprintf(full_path, "%s%s", base_dir, path);
-    printf("getattr: %s\n", full_path);
-    res = lstat(full_path, stat);
+    printf("getattr: %s\n", path);
+    res = lstat(path, stat);
     if (res == -1)
         return -errno;
 
@@ -31,9 +32,9 @@ int stackfs__open(const char *path, struct fuse_file_info *fi)
     char full_path[PATH_MAX];
 
     sprintf(full_path, "%s%s", base_dir, path);
-    printf("open: %s\n", full_path);
+    printf("open: %s\n", path);
 
-    fd = open(full_path, fi->flags);
+    fd = open(path, fi->flags);
 
     if (fd == -1)
         return -errno;
@@ -49,10 +50,10 @@ int stackfs__opendir(const char *path, struct fuse_file_info *fi)
     char full_path[PATH_MAX];
 
     sprintf(full_path, "%s%s", base_dir, path);
-    printf("opendir: %s \n", full_path);
+    printf("opendir: %s \n", path);
 
     // Open directory
-    dir = opendir(full_path);
+    dir = opendir(path);
     if (dir == NULL)
     {
         ret = -errno;
@@ -72,7 +73,7 @@ int stackfs__read(const char *path, char *buff, size_t size, off_t offset, struc
     char full_path[PATH_MAX];
 
     sprintf(full_path, "%s%s", base_dir, path);
-    printf("read: %s\n", full_path);
+    printf("read: %s\n", path);
 
     res = pread(fi->fh, buff, size, offset);
     if (res == -1)
@@ -81,7 +82,7 @@ int stackfs__read(const char *path, char *buff, size_t size, off_t offset, struc
     return res;
 }
 
-int stackfs__readdir(const char *path, void *buff, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi,enum fuse_readdir_flags flags)
+int stackfs__readdir(const char *path, void *buff, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi, enum fuse_readdir_flags flags)
 {
     DIR *dp;
     struct dirent *de;
@@ -92,9 +93,9 @@ int stackfs__readdir(const char *path, void *buff, fuse_fill_dir_t filler, off_t
     char full_path[PATH_MAX];
 
     sprintf(full_path, "%s%s", base_dir, path);
-    printf("readdir: %s\n", full_path);
+    printf("readdir: %s\n", path);
 
-    dp = opendir(full_path);
+    dp = opendir(path);
     if (dp == NULL)
         return -errno;
 
@@ -110,7 +111,7 @@ int stackfs__readdir(const char *path, void *buff, fuse_fill_dir_t filler, off_t
             break;
     }
 
-    closedir(dp);
+    
     return 0;
 }
 
@@ -121,13 +122,45 @@ int stackfs__readlink(const char *path, char *buff, size_t size)
     char full_path[PATH_MAX];
 
     sprintf(full_path, "%s%s", base_dir, path);
-    printf("readlink: %s\n", full_path);
+    printf("readlink: %s\n", path);
 
-    ret = readlink(full_path, buff, size - 1);
+    ret = readlink(path, buff, size - 1);
     if (ret == -1)
         return -errno;
 
     buff[ret] = '\0';
+
+    return 0;
+}
+
+int stackfs__read_buf(const char *path, struct fuse_bufvec **bufp,
+                      size_t size, off_t off, struct fuse_file_info *fi)
+{
+    (void)path;
+    struct fuse_bufvec *buf;
+
+    buf = malloc(sizeof(struct fuse_bufvec));
+
+    if (buf == NULL)
+        return -ENOMEM;
+
+    *buf = FUSE_BUFVEC_INIT(size);
+
+    buf->buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;
+    buf->buf[0].fd = fi->fh;
+    buf->buf[0].pos = off;
+
+    *bufp = buf;
+
+    printf("read_buf: %s\n", path);
+
+
+    return 0;
+}
+
+int stackfs__releasedir (const char *path, struct fuse_file_info *fi){
+    closedir((DIR *)fi->fh);
+    printf("releasedir: %s\n", path);
 
     return 0;
 }
@@ -139,7 +172,9 @@ static struct fuse_operations stackfs__op = {
     .read = stackfs__read,
     .readdir = stackfs__readdir,
     .readlink = stackfs__readlink,
-
+    .read_buf = stackfs__read_buf,
+    .releasedir = stackfs__releasedir,
+    
 };
 
 int main(int argc, char *argv[])
